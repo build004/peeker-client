e92da8e3a463357121ed73b9d61a73b0033609c0
chany <lyhy0310@gmail.com>
2018-04-21 15:14:47 +0900

    [modify] signup screen 이동

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input, Button, Text, SocialIcon } from 'react-native-elements';
 import { Formik, FormikProps, FormikActions } from 'formik';
+import Signup from './screens/Signup/Signup';
 
 import * as api from './service/api/auth';
 
@@ -17,38 +18,7 @@ export default class App extends React.Component<{}> {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text h3>Sign Up</Text>
-				<Formik
-					onSubmit={(values: FormValues) => {
-						console.log(values);
-						api.signUp(values.email, values.password);
-					}}
-					render={(props: FormikProps<FormValues>) => (
-						<View>
-							<TextInput
-								name="email"
-								onChangeText={props.setFieldValue}
-								value={props.values.email}
-								autoCapitalize="none"
-								keyboardType="email-address"
-							/>
-							<TextInput
-								name="password"
-								onChangeText={props.setFieldValue}
-								value={props.values.password}
-								secureTextEntry
-								autoCapitalize="none"
-							/>
-							<Button title="submit" onPress={props.handleSubmit} />
-						</View>
-					)}
-				/>
-				<SocialIcon
-					title="Sign Up With Facebook"
-					button
-					type="facebook"
-					style={{ padding: 15 }}
-				/>
+				<Signup/>
 			</View>
 		);
 	}
